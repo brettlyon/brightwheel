b33687a1eb4195d2594b18d0cd88e6859c166556
Brett Lyon <brett.lyon.87@gmail.com>
2015-09-25 02:02:43 -0700

    Initial Commit

---
+++ Brightwheel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Brightwheel
+//
+//  Created by Brett Lyon on 9/25/15.
+//  Copyright (c) 2015 BrettLyon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
